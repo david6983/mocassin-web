26c6ee12a1532bbf865bed30839752e1223e72d2
david haioum <dav6983@gmail.com>
2020-11-22 16:07:01 +0100

    fix: clear master branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
 
-    <h1>
-      Welcome to {{title}}!
-    </h1>
-    <button pButton type="button" label="Click" (click)="handleClick()" ></button>
+
     <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'mocassin-web-app';
-
-  handleClick(): void {
-    console.log('hello primeng');
-  }
+  title = 'Mocassin';
 }
